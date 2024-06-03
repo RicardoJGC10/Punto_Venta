008edd5b2d8cfe4b8da627e2e1cd0782662c52fc
RicardoJGC10 <chatotote10@gmail.com>
2024-06-02 20:54:28 -0600

    Se agrega el Proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'punto_venta' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('punto_venta');
+    expect(app.title).toEqual('Punto_Venta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, punto_venta');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Punto_Venta');
   });
 });
